1550fde5b1c22672927e1907e7cb6514c370d092
Manas Gupta <mmanasggupta@gmail.com>
2022-06-05 19:30:48 +0530

    add matrix

--- pages/company/rc/[rcId]/proforma/[proformaId]/step2.tsx
+++ pages/company/rc/[rcId]/proforma/[proformaId]/step2.tsx
@@ -7,8 +7,13 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import ActiveButton from "@components/Buttons/ActiveButton";
-import InactiveButton from "@components/Buttons/InactiveButton";
+// import ActiveButton from "@components/Buttons/ActiveButton";
+// import InactiveButton from "@components/Buttons/InactiveButton";
+import Checkbox from "@mui/material/Checkbox";
+
+const binMatrix = Array(12)
+  .fill(0)
+  .map(() => Array(22).fill(0));
 
 function Step2() {
   return (
@@ -32,21 +37,30 @@ function Step2() {
           <h2>Step 2/5 Basic Details</h2>
           <TableContainer
             component={Paper}
-            sx={{ minWidth: 330, maxWidth: 700 }}
+            sx={{ minWidth: 330, maxWidth: 100 }}
           >
             <Table
-              sx={{ minWidth: 300, maxWidth: 700, backgroundColor: "#ebebeb" }}
+              sx={{ minWidth: 300, maxWidth: 1900, backgroundColor: "#ebebeb" }}
               aria-label="simple table"
             >
               <TableBody>
-                <TableRow>
+                {binMatrix.map((row) => (
+                  <TableRow key="">
+                    {row.map(() => (
+                      <TableCell key="" component="th" scope="row">
+                        <Checkbox />
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+                {/* <TableRow>
                   <TableCell align="center">
                     <ActiveButton sx={{ width: 100 }}>Next</ActiveButton>
                   </TableCell>
                   <TableCell align="center">
                     <InactiveButton sx={{ width: 100 }}>Reset</InactiveButton>
                   </TableCell>
-                </TableRow>
+                </TableRow> */}
               </TableBody>
             </Table>
           </TableContainer>
